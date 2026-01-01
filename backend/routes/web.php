5451e342c4b58b8016495ca948f495ee235c56db
binhabc <dothaibinh021218@gmail.com>
2026-01-01 23:43:34 +0700

    dashboard chưa test do không có data

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,12 +3,13 @@
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin', fn() => view('admin.dashboard.index'));
+//Route::get('/admin', fn() => view('admin.dashboard.index'));
 Route::get('/admin/products', fn() => view('admin.products.index'));
 Route::get('/admin/products/create', fn() => view('admin.products.create'));
 Route::get('/admin/products/{id}/edit', fn() => view('admin.products.edit'));
@@ -34,4 +35,12 @@ Route::prefix('admin')
     ->name('admin.')
     ->group(function () {
         Route::resource('user', UserController::class);
+    });
+
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+         //Route::resource('dashboard', DashboardController::class);
     });
