51cfc364f2890250c6474f4b1b1946a493699040
diepvanhieu25-tech <diepvanhieu25@gmail.com>
2026-01-04 16:56:35 +0700

    update api get brands and delete img

--- backend/app/Http/Resources/Api/BrandResource.php
+++ backend/app/Http/Resources/Api/BrandResource.php
@@ -8,13 +8,22 @@ use Illuminate\Support\Facades\Storage;
 
 class BrandResource extends JsonResource
 {
-
     public function toArray(Request $request): array
     {
+        $logo = null;
+
+        if ($this->logo_url) {
+            if (filter_var($this->logo_url, FILTER_VALIDATE_URL)) {
+                $logo = $this->logo_url;
+            } else {
+                $logo = asset(Storage::url($this->logo_url));
+            }
+        }
+
         return [
-            'id'       => $this->id,
-            'name'     => $this->name,
-            'logo_url' => $this->logo_url ? Storage::url($this->logo_url) : null,
+            'value'    => $this->id,
+            'label'    => $this->name,
+            'logo'     => $logo,
         ];
     }
 }
